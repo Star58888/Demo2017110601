c369fd599ff2762a2561f857d8e2a3e76c20d67b
star <s0913940588@gmail.com>
2017-11-08 13:31:33 +0800

    updata

--- app/src/main/java/com/star/demo2017110601/DetailActivity.java
+++ app/src/main/java/com/star/demo2017110601/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.star.demo2017110601;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -14,18 +17,47 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        tvId = findViewById(R.id.textView);
-        tvName = findViewById(R.id.textView2);
-        tvTel = findViewById(R.id.textView3);
-        Intent intent =  getIntent();
-        id = intent.getIntExtra("id" , -1);
+        tvId = (TextView) findViewById(R.id.textView);
+        tvName = (TextView) findViewById(R.id.textView2);
+        tvTel = (TextView) findViewById(R.id.textView3);
+        Intent it = getIntent();
+        id = it.getIntExtra("id", -1);
         SQLiteDatabase db = SQLiteDatabase.openDatabase(DBInfo.DB_FILE, null, SQLiteDatabase.OPEN_READWRITE);
-        Cursor c = db.query("phone" , new String[]{"id" , "username" , "tel"},"id=?",new String[] {String.valueOf(id)} ,null , null , null);
+        Cursor c = db.query("phone", new String[] {"id", "username", "tel"}, "id=?", new String[] {String.valueOf(id)}, null, null, null);
         if (c.moveToFirst())
         {
             tvId.setText(String.valueOf(c.getInt(0)));
             tvName.setText(c.getString(1));
             tvTel.setText(c.getString(2));
         }
+
     }
+    public void clickBack(View v)
+    {
+        finish();
+    }
+    public void clickDelete(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
+        builder.setTitle("刪除確認");
+        builder.setMessage("請確認刪除");
+        builder.setPositiveButton("刪除", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SQLiteDatabase db = SQLiteDatabase.openDatabase(DBInfo.DB_FILE, null, SQLiteDatabase.OPEN_READWRITE);
+                db.delete("phone", "id=?", new String[] {String.valueOf(id)});
+                finish();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+
+    }
+
+
 }
