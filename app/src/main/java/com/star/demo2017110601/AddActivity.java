de0f5ef6ce485d9d5c1fa88b631dff03d1de7cb5
star <s0913940588@gmail.com>
2017-11-08 10:36:34 +0800

    updata

--- app/src/main/java/com/star/demo2017110601/AddActivity.java
+++ app/src/main/java/com/star/demo2017110601/AddActivity.java
@@ -1,7 +1,12 @@
 package com.star.demo2017110601;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.star.demo2017110601.DBInfo.DB_FILE;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+    public void clickAdd(View v) {
+        SQLiteDatabase db = SQLiteDatabase.openDatabase(DB_FILE, null, SQLiteDatabase.OPEN_READWRITE);
+        EditText ed = findViewById(R.id.editText);
+        EditText ed2 = findViewById(R.id.editText2);
+        String username = ed.getText().toString();
+        String tel = ed2.getText().toString();
+        String str = "Insert Into phone (username , tel) values ('" + username + "', '"+ tel + "')";
+        db.execSQL(str);
+        finish();
     }
 }
---
+++ app/src/main/java/com/star/demo2017110601/DBInfo.java
@@ -0,0 +1,9 @@
+package com.star.demo2017110601;
+
+/**
+ * Created by Star on 2017/11/8.
+ */
+
+public class DBInfo {
+    public static String DB_FILE;
+}
